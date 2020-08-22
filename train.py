678b779bf58503c3b8f6b1d76d16fadcf360e3da
tanacchi <q111026d@mail.kyutech.jp>
2020-08-22 16:42:09 +0900

    Update

--- train.py
+++ train.py
@@ -3,9 +3,11 @@ from pydub import AudioSegment
 from glob import glob
 from sklearn.model_selection import train_test_split
 import keras
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.layers import Dense,  Flatten, Input
-from keras.layers import Conv1D, MaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, LSTM
+from keras.layers import Activation
+from keras.optimizers import Adam
 import os
 
 
@@ -16,34 +18,24 @@ def wav_to_array(file):
     return song_arr
 
 import time
-files = list(glob("./output/*.wav"))[::5]
-data_size = wav_to_array(files[0]).shape[0]
-file_batch = 50
-X = np.zeros((0, data_size, 1))
-for i in range(0, len(files), file_batch):
-    mini_files = files[i: i+file_batch]
-    x = [wav_to_array(file) for file in mini_files]
-    x = list(filter(lambda x: x.shape[0] == data_size, x))
-    x = np.array(x)
-    x = x.reshape((x.shape[0], x.shape[1], 1))
-    X = np.concatenate([X, x])
-
+files = list(glob("./output/*.wav"))[::4]
+X = np.array([wav_to_array(file) for file in files])
+X = X.reshape((X.shape[0], X.shape[1], 1))
 
 filenames = [os.path.basename(file) for file in files]
-print(filenames[0])
-print(filenames[0][0])
 labels = [int(filename[0]) for filename in filenames]
 print(labels)
-y = np.array(labels)
 print(len(files))
+outputs = 2
+y = np.array([np.identity(outputs)[l] for l in labels])
 print(X.shape)
 print(y.shape)
-outputs = 1
+print(y)
 
 
 random_state = 42
-train_X, test_X = X[50:], X[:10]
-train_y, test_y = y[50:], y[:10]
+train_X, test_X = X[50:], X[:50]
+train_y, test_y = y[50:], y[:50]
 # train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
 features = train_X.shape[1]
@@ -63,13 +55,14 @@ x = Flatten() (x)
 x = Dense(100, activation='relu') (x) #（ユニット数）
 x_outputs = Dense(outputs, activation='sigmoid', name='x_outputs') (x)
 
+batch_size = 50
 model = Model(inputs=x_inputs, outputs=x_outputs)
-model.compile(optimizer='adam',
+model.compile(optimizer='sgd',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(train_X, train_y, batch_size=600, epochs=50)
+model.fit(train_X, train_y, batch_size=batch_size, epochs=50)
 
 from sklearn.metrics import roc_auc_score
-pred_y_x1 = model.predict(test_X, batch_size=50)
+pred_y_x1 = model.predict(test_X, batch_size=batch_size)
 print(pred_y_x1)
 print(roc_auc_score(test_y, pred_y_x1))
